ca3374cbe1aa726aace3d0a9478876c23ca36602
Alexander Bluhm <alexander.bluhm@gmx.net>
2014-08-10 20:54:13 +0200

    Revert "Change root to /var/empty." There is no resolv.conf so getaddrinfo() does not wrok.
    This reverts commit 42aa5b5b505111841f6a95e587157bb4663ea5f9.

--- util.c
+++ util.c
@@ -27,7 +27,6 @@
 #include <err.h>
 #include <event.h>
 #include <limits.h>
-#include <paths.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -98,8 +97,6 @@ droppriv(void)
 {
 	const char	*sudoid, *errstr;
 
-	if (chroot(_PATH_VAREMPTY) == -1)
-		err(1, "chroot %s", _PATH_VAREMPTY);
 	if ((sudoid = getenv("SUDO_GID")) != NULL) {
 		gid_t		 gid;
 
